c243a2bdd338e4ddc36204165df7c49dbcc7985b
李学凯 <cmshome@163.com>
2019-12-12 09:37:48 +0800

    一个get请求

--- src/main/java/com/lxk/controller/TestController.java
+++ src/main/java/com/lxk/controller/TestController.java
@@ -28,6 +28,8 @@ public class TestController {
     @Value("${citic_ip_info_json_result:{\"returnCode\":\"1\",\"totalNum\":1000,\"returnNum\":100,\"returnMsg\":[{\"mainName\":\"短信平台\",\"sysName\":\"控股短信平台\",\"ipAddress\":\"1.1.1.1\"},{\"sysName\":\"分行网络-长春分行\",\"ipAddress\":\"2.2.2.2\"},{\"sysName\":\"分行网络-北京分行\",\"ipAddress\":\"3.3.3.3\"},{\"sysName\":\"分行网络-南京分行\",\"ipAddress\":\"4.4.4.4\"},{\"sysName\":\"分行网络-xx分行\",\"ipAddress\":\"5.5.5.5\"},{\"sysName\":\"分行网络-yy分行\",\"ipAddress\":\"6.6.6.6\"},{\"mainName\":\"\",\"sysName\":\"分行网络-zz分行\",\"ipAddress\":\"7.7.7.7\"},{}]}}")
     private String jsonMap;
 
+    private static final String json = "{\"pageNo\":1,\"pageSize\":1000,\"totalPage\":1,\"total\":146,\"records\":[{\"userId\":\"452ca0aadd904245b0fee55671fa788d\",\"tenantId\":\"e10adc3949ba59abbe56e057f20f88dd\",\"realname\":\"shui\",\"account\":\"shui123\",\"email\":\"123456@qq.com\",\"mobile\":\"18899963\",\"passwd\":null,\"skin\":null,\"lockStatus\":null,\"lockEndTime\":null,\"accountExpire\":null,\"passwdExpire\":1575968162000,\"imgPath\":\"/tenant/userimages/default.png\",\"crossUserId\":null,\"type\":null,\"position\":null,\"empId\":null,\"ext\":null,\"addTime\":1575968162000,\"status\":null,\"apiKeys\":null,\"sex\":null,\"tel\":null,\"openId\":null,\"wxStatus\":null,\"language\":null,\"id\":null},{\"userId\":\"205dd1bc5ba74a9a824304fdc190612d\",\"tenantId\":\"e10adc3949ba59abbe56e057f20f88dd\",\"realname\":\"shui\",\"account\":\"bigMax\",\"email\":\"123456@qq.com\",\"mobile\":\"18899964\",\"passwd\":null,\"skin\":null,\"lockStatus\":1,\"lockEndTime\":null,\"accountExpire\":null,\"passwdExpire\":1575964173000,\"imgPath\":\"/tenant/userimages/default.png\",\"crossUserId\":null,\"type\":null,\"position\":null,\"empId\":null,\"ext\":null,\"addTime\":1575964173000,\"status\":null,\"apiKeys\":null,\"sex\":null,\"tel\":null,\"openId\":null,\"wxStatus\":null,\"language\":null,\"id\":null},{\"userId\":\"c428ddd0f66c4c338e878021056090c0\",\"tenantId\":\"e10adc3949ba59abbe56e057f20f88dd\",\"realname\":\"shui\",\"account\":\"LxkLxkLxk\",\"email\":\"123456@qq.com\",\"mobile\":\"18899964523\",\"passwd\":null,\"skin\":null,\"lockStatus\":null,\"lockEndTime\":null,\"accountExpire\":null,\"passwdExpire\":1575963992000,\"imgPath\":\"/tenant/userimages/default.png\",\"crossUserId\":null,\"type\":null,\"position\":null,\"empId\":null,\"ext\":null,\"addTime\":1575963992000,\"status\":null,\"apiKeys\":null,\"sex\":null,\"tel\":null,\"openId\":null,\"wxStatus\":null,\"language\":null,\"id\":null}]}";
+
 
     /**
      * init：初始化页面
@@ -48,6 +50,23 @@ public class TestController {
         }
         System.out.println("get 请求到此一游。。。");
         return new JsonResult(true, "lxk", MAP);
+    }
+
+    /**
+     * init：初始化页面
+     */
+    @ResponseBody
+    @RequestMapping(value = "getUsers", method = RequestMethod.GET)
+    public String getUsers(@RequestParam(required = false, value = "tenant_id") String tenantId,
+                           @RequestParam(required = false, value = "page_size") Integer pageSize) {
+        LOG.debug("---------------TestController: getUsers---------------");
+
+        System.out.println("http get ");
+        System.out.println("tenantId：" + tenantId);
+        System.out.println("pageSize：" + pageSize);
+        System.out.println("get 请求到此一游。。。");
+
+        return json;
     }
 
     /**
