74b335cf6694115dea65cd891fe08a9d704f563e
李学凯 <cmshome@163.com>
2019-12-13 15:55:13 +0800

    HandlerInterceptorAdapter 中 使用 @Resource 注入bean

---
+++ src/main/java/com/lxk/service/TestService.java
@@ -0,0 +1,17 @@
+package com.lxk.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * test
+ *
+ * @author LiXuekai on 2019/12/13
+ */
+@Service
+public class TestService {
+
+
+    public void test(){
+        System.out.println("-------");
+    }
+}
--- src/main/java/com/lxk/utils/interceptor/ApplicationInterceptor.java
+++ src/main/java/com/lxk/utils/interceptor/ApplicationInterceptor.java
@@ -1,8 +1,10 @@
 package com.lxk.utils.interceptor;
 
 import com.lxk.httpModel.SessionInfo;
+import com.lxk.service.TestService;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,6 +13,10 @@ import javax.servlet.http.HttpServletResponse;
  * 该类负责拦截用户请求
  */
 public class ApplicationInterceptor extends HandlerInterceptorAdapter {
+
+    @Resource
+    private TestService testService;
+
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -24,7 +30,8 @@ public class ApplicationInterceptor extends HandlerInterceptorAdapter {
         String servletPath = request.getServletPath();
         //参数
         String accessParam = request.getQueryString();
-        //System.out.println("项目名称，根路径" + root + ";访问路径" + accessPath + ";参数" + ";相对路径" + servletPath + accessParam);
+        System.out.println("项目名称，根路径" + root + ";访问路径" + accessPath + ";参数" + ";相对路径" + servletPath + accessParam);
+        testService.test();
         return true;
     }
 }
